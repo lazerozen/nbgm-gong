e94e0263bed18b9ce2b32fbc8f8dcf0edaac3038
Wolfgang Riedl <wolfgang.riedl@siemens.com>
2019-07-25 10:58:36 +0200

    Zweiter commit, haha.

---
+++ Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NAudio.Wave;
+
+namespace NbgmGong
+{
+    public partial class Form1 : Form
+    {
+        private readonly WaveOut _waveOut;
+        private bool _isPlaying = false;
+        private Mp3FileReader _mp3Reader;
+        public Form1()
+        {
+            InitializeComponent();
+            _waveOut = new WaveOut();
+
+            //_mp3Reader = new Mp3FileReader(new MemoryStream(Properties.Resources.Gong_Variante_1));
+            //_waveOut = new WaveOut(); // or WaveOutEvent()
+            //_waveOut.Init(_mp3Reader);
+        }
+
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+            if (_waveOut.PlaybackState == PlaybackState.Playing)
+                return;
+
+            _mp3Reader = new Mp3FileReader(new MemoryStream(Properties.Resources.Gong_Variante_1));
+            _waveOut.Init(_mp3Reader);
+            _waveOut.Play();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # nbgm-gong
 Ein Gong, ein Gong.
+
+Braucht .NET 4.7.2
+Einfach entpacken und .exe doppelklicken.
